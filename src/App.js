ee073422c5b49b1212ba6ca3cfb469ef1f45393f
RomanHrytsko <romangritsko@gmail.com>
2020-12-08 17:08:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, {Component} from 'react';
 
-import AllUsersComponents from "./components/AllUsersComponents/AllUsersComponents";
-import AllCommentsComponents from "./components/AllComentsComponent/AllCommentsComponents";
-import AllPosts from "./components/AllPostsComponent/AllPosts";
-import AppPhotosComponent from "./components/AllPhotos/AppPhotosComponent";
-import PostComponent from "./components/AllPostsComponent/PostComponent";
-
 
 class App extends Component {
 
@@ -13,7 +7,7 @@ class App extends Component {
         return (
             <div>
                 {
-                    <AppPhotosComponent/>
+
                 }
             </div>
         )

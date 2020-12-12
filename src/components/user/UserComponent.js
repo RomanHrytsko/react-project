393d7c9a1c4b7ec8d003817b824ab508fc537d90
RomanHrytsko <romangritsko@gmail.com>
2020-12-12 18:50:22 +0200

    lesson 5 started

--- src/components/user/UserComponent.js
+++ src/components/user/UserComponent.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 import './usetCss.css'
+
 class UserComponent extends Component {
     render() {
-        let {item, clsName} = this.props
 
 
         return (
 
-            <div className={clsName}>
-                <h3>
-                    {item.name}-{item.age}-{item.status.toString()}
-                </h3>
+            <div>
+
             </div>
         );
     }

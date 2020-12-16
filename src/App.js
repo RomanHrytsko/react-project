45ddbfee3a930849dee7655ac396b5c7037e9ed8
RomanHrytsko <romangritsko@gmail.com>
2020-12-16 15:35:56 +0200

    task from lesson 6 ClassWork p2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, {Component} from 'react';
-import UserComponent from "./components/user/UserComponent";
 
 
 class App extends Component {
-    users = [
-  { name: "vasya", age: 31, status: false },
-  { name: "petya", age: 30, status: true },
-  { name: "kolya", age: 29, status: true },
-  { name: "olya", age: 28, status: false },
-  { name: "max", age: 30, status: true },
-  { name: "anya", age: 31, status: false },
-  { name: "oleg", age: 28, status: false },
-  { name: "andrey", age: 29, status: true },
-  { name: "masha", age: 30, status: true },
-  { name: "olya", age: 31, status: false },
-  { name: "max", age: 31, status: true },
-];
+    myForm = React.createRef()
+state = {inputValue: ''}
+
     render() {
+        let {inputValue} = this.state
+        console.log(inputValue)
         return (
             <div>
-                {
-                    this.users.map((value, index)=>{
-                        let name = 'target'
-                        if(index %2){
-                            name = 'point'
-                        }
-                    return(<UserComponent item={value} key={index} clsName={name}/>)
-                })
-                }
+                <form action="" onSubmit={this.send} ref={this.myForm}>
+
+                    <input onInput={this.commitState} value={this.state.inputValue}/> <button>send</button>
+                </form>
+
+
             </div>
-        )
+        );
     }
+   send=(e)=>{
+        e.preventDefault()
+       // console.log(this.myForm.current[0].value)
+       // console.log(e.target.children[0].value)
+   }
+   commitState= (e)=>{
+        this.setState({inputValue: e.target.value})
+       // console.log(e.target.value)
+   }
 }
 
 export default App;

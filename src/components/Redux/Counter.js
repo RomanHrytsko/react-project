4aeb65482928a7432721ce1e014e5596d432a3d8
RomanHrytsko <romangritsko@gmail.com>
2020-12-24 22:57:09 +0200

    Redux part1 finished

--- src/components/Redux/Counter.js
+++ src/components/Redux/Counter.js
@@ -1,13 +1,15 @@
 import React from "react";
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 export default function Counter() {
-    const counter = useSelector((state) => {
-        console.log(state)
-    })
+    const counter = useSelector(({counter}) => counter)
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Hello {counter}</h2>
+            <button onClick={()=> dispatch({type: "INC_COUNTER"})}>inc</button>
+            <button onClick={()=> dispatch({type: "DEC_COUNTER"})}>dec</button>
+            <button onClick={()=> dispatch({type: "RESET"})}>res</button>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const initialState = {
+    counter: 0
+}
+const reducer = (state = initialState, action)=>{
+   switch (action.type){
+       case 'INC_COUNTER': {
+           return{...state, counter: state.counter + 1}
+       }
+       case 'DEC_COUNTER': {
+           return{...state, counter: state.counter - 1}
+       }
+       case 'RESET': {
+           return{...state, counter: 0}
+       }
+       default:{
+           return state
+       }
+   }
+}
+const store = createStore(reducer)
+
 ReactDOM.render(
+
   <React.StrictMode>
+    <Provider store={store}>
+
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

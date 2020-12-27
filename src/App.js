e619775aeba45c0886511b2f7d32d365d95cc79c
RomanHrytsko <romangritsko@gmail.com>
2020-12-27 23:50:54 +0200

    lesson 9 in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, {Component} from 'react';
-import UserComponent from "./redux/user/UserComponent";
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {INC_COUNTER, RESET_COUNTER, DEC_COUNTER} from "./redux/action-types";
 
+import {incCounter,decCounter,resetCounter} from "./redux/action-creators";
 
-class App extends Component {
+export default function App() {
 
-    render() {
-        return (
-            <div>
-                {
+    // const {todos, counter} = useSelector(({counter: {counter}}, {todos: {todos}}) => ({counter, todos}))
+    const test = useSelector((state) => {
+        console.log(state)
+        return state
+        }
+    )
+    const counter = useSelector(({counter})=> counter.counter)
+    const todos = useSelector(({todos})=> todos.todos)
+    const storeData = useSelector(({counter:{counter}, todos:{todos}}) => ({todos, counter}))
 
-                }
-            </div>
-        )
-    }
+    const incCounter = () => ({type: 'INC_COUNTER'})
+    const decCounter = () => ({type: 'DEC_COUNTER'})
+    const resetCounter = () => ({type: 'RESET_COUNTER'})
+
+    const dispatch = useDispatch()
+    const handlerInc = () => dispatch(incCounter())
+    const handlerDec = () => dispatch(decCounter())
+    const handlerReset = () => dispatch(resetCounter())
+
+    return (
+        <div>
+
+            <h2>Counter: {storeData}</h2>
+            <button onClick={handlerInc}>Inc</button>
+            <button onClick={handlerDec}>Dec</button>
+            <button onClick={handlerReset}>Reset</button>
+
+        </div>
+    );
 }
-
-export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux'
+import counterReducer from './counter-reducer'
+import todosReducer from './todos-reducer'
+export const reducer = combineReducers({
+    counter: counterReducer,
+    todos: todosReducer,
+})

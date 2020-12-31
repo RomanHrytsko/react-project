7de1f33c125ae95ec36d24a53a0452081b28ea0f
Roman Hrytsko <72077761+RomanHrytsko@users.noreply.github.com>
2020-12-31 14:15:54 +0200

    Delete index.js

--- src/redux/reduces/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import cartReducer from './cart-reducer'
-import wishListReducer from './wishListReducer'
-import itemListReducer from './item-list-reducer'
-export const reducer = combineReducers({
-    cart: cartReducer,
-    wishList: wishListReducer,
-    itemList: itemListReducer
-})
